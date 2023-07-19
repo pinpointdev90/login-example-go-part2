66ae47507b35d98a2c6ab16387992a8b265c367a
pinpointdev90 <tl9721lt@gmail.com>
2023-07-20 00:11:15 +0900

    login-exampleの追加

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, world!\n")
+	})
+
+	e.Logger.Fatal(e.Start(":8000"))
+}
+
